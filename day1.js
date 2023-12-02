5c58d7d3249f656ce0dce63d9d02cb743c34cfae
Gabe Durand <gdurand@jlgarchitects.com>
2023-12-02 01:03:56 -0600

    day1 solution part 2

--- day1.js
+++ day1.js
@@ -2,11 +2,29 @@ const fs = require("fs");
 
 const puzzelInput = fs.readFileSync("day1.txt", "utf-8");
 let total = 0;
+const validDigits = [
+  "one",
+  "two",
+  "three",
+  "four",
+  "five",
+  "six",
+  "seven",
+  "eight",
+  "nine",
+];
 puzzelInput.split(/\r?\n/).forEach((line) => {
   if (line) {
-    // remove all letter
+    validDigits.forEach((d, i) => {
+      if (line.includes(d)) {
+        line = line.replaceAll(d, `${d}${i + 1}${d}`);
+      }
+    });
+
     const calibration = line.replace(/\D/g, "");
-    total += Number(calibration[0] + calibration.slice(-1));
+    const numbers = Number(calibration[0] + calibration.slice(-1));
+    console.log(numbers);
+    total += numbers;
   }
 });
 
